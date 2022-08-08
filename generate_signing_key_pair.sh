3a82aafba9e485001da1d9028a2afb52a4447704
Joshua Evans <x.evans@gmail.com>
2022-08-08 15:58:11 -0400

    Create CA out of TF to avoid State file.

---
+++ generate_signing_key_pair.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+step certificate create root.linkerd.cluster.local ca.crt ca.key \
+    --profile root-ca --no-password --insecure &&
+    kubectl create secret tls linkerd-trust-anchor \
+    --cert=ca.crt --key=ca.key --namespace=linkerd
--- namespaces.tf
+++ namespaces.tf
@@ -17,4 +17,16 @@ resource "kubernetes_namespace" "linkerd" {
       team = "ops",
     }
   }
+
+  # Avoid saving this secret in the TF State File.
+  # Requires the Step executable locally to create a signing key pair.
+  # View warning: https://registry.terraform.io/providers/hashicorp/tls/latest/docs#secrets-and-terraform-state
+  provisioner "local-exec" {
+    working_dir = "/tmp/"
+    command     = <<-EOT
+      cp ../../${path.module}/generate_signing_key_pair.sh /tmp/
+      chmod u+x /tmp/generate_signing_key_pair.sh
+      bash /tmp/generate_signing_key_pair.sh
+    EOT
+  }
 }
